4196b7144c2936728ec6426a4beb5c255fcf342f
Matt Rosanio <mmrosani@syr.edu>
2016-05-18 08:45:08 -0700

    Add comments and reorganize code

--- app/src/main/java/com/epicodus/simplegame/views/GameView.java
+++ app/src/main/java/com/epicodus/simplegame/views/GameView.java
@@ -22,36 +22,33 @@ import com.epicodus.simplegame.models.Seaweed;
 import java.util.ArrayList;
 import java.util.Random;
 
-/**
- * Created by Guest on 5/16/16.
- */
 public class GameView extends SurfaceView implements Runnable {
+    public static final String TAG = GameView.class.getSimpleName();
+
+    //Game State Constants
     public static final int GAME_START = 0;
     public static final int GAME_PLAYING = 1;
     public static final int GAME_UPGRADING = 2;
     public static final int GAME_OVER = 3;
-    public static final String TAG = GameView.class.getSimpleName();
+
+    //Game Essentials
     Thread gameThread = null;
     SurfaceHolder ourHolder;
     volatile boolean playing;
     Canvas canvas;
     Paint paint;
     long fps;
-    Bitmap fillBubbleMeter;
-    Bitmap bubbleMeter;
-    private long timeThisFrame;
-    boolean isMoving = false;
-    boolean isShooting = false;
-    float swimSpeedPerSecond = 150;
-    float playerXPosition = 10;
-    float playerYPosition = 400;
-    float scrollSpeed = 40;
+    private
     float screenX;
     float screenY;
+    int gameState;
+    int score;
+    Context mContext;
+
+    //Joystick variables
     float pointerX;
     float pointerY;
     float circleXPosition;
-    int gameState;
     float circleYPosition;
     float circleDefaultX;
     float circleDefaultY;
@@ -60,15 +57,22 @@ public class GameView extends SurfaceView implements Runnable {
     float distance;
     float theta;
     float joystickRadius;
-    int joystickPointerId;
+
+    //Models
     Player player;
     ArrayList<Harpoon> harpoons = new ArrayList<>();
     ArrayList<Dolphin> dolphins = new ArrayList<>();
     ArrayList<Seaweed> seaweeds = new ArrayList<>();
     Bubble bubble;
+
+    //Bitmaps/animation variables
+    Bitmap fillBubbleMeter;
+    Bitmap bubbleMeter;
+    boolean isMoving = false;
+
+    //Other
+    float scrollSpeed;
     Random randomNumberGenerator;
-    Context mContext;
-    int score;
 
     public GameView(Context context, float x, float y) {
         super(context);
@@ -78,17 +82,25 @@ public class GameView extends SurfaceView implements Runnable {
         paint = new Paint();
         screenX = x;
         screenY = y;
+
+        //Setup Joystick
         circleDefaultX = (float) (0.15*screenX);
         circleDefaultY = (float) (0.78*screenY);
         pointerX = circleDefaultX;
         pointerY = circleDefaultY;
         joystickRadius = (float) .1*screenY;
+
+        //Initialize camera movement
+        scrollSpeed = screenX/20;
     }
 
     public void prepareLevel(Context context) {
+        //clear variables
         dolphins.clear();
         harpoons.clear();
+        seaweeds.clear();
 
+        //Initialize Models
         player = new Player(context, screenX, screenY);
 
         for (int i=0; i < 10; i++) {
@@ -102,26 +114,26 @@ public class GameView extends SurfaceView implements Runnable {
             harpoons.add(new Harpoon(context, screenX, screenY));
         }
 
-        joystickPointerId = -1;
-
-        randomNumberGenerator = new Random();
-
         for (int i = 0; i < 4; i++) {
             dolphins.add(new Dolphin(context, screenX, screenY));
         }
 
         bubble = new Bubble(screenX, screenY, context);
+
+        //Setup game variables
+        randomNumberGenerator = new Random();
         score = 0;
 
+        //Setup Bitmaps
         bubbleMeter = BitmapFactory.decodeResource(getResources(), R.drawable.bubblemeter);
         bubbleMeter = Bitmap.createScaledBitmap(bubbleMeter, (int) screenX/40, (int) screenY/30, false);
-
         fillBubbleMeter = BitmapFactory.decodeResource(getResources(), R.drawable.fillbubblemeter);
         fillBubbleMeter = Bitmap.createScaledBitmap(fillBubbleMeter, (int) screenX/40, (int) screenY/30, false);
     }
 
     @Override
     public void run() {
+        long timeThisFrame;
         while(playing) {
             long startFrameTime = System.currentTimeMillis();
             update();
@@ -136,7 +148,8 @@ public class GameView extends SurfaceView implements Runnable {
     public void update() {
 
         if(gameState == GAME_PLAYING) {
-            playerXPosition = playerXPosition + (swimSpeedPerSecond / fps);
+
+            //Calculate joystick position
             deltaX = pointerX-circleDefaultX;
             deltaY = pointerY-circleDefaultY;
             distance = (float) Math.sqrt((deltaX*deltaX) + (deltaY*deltaY));
@@ -150,6 +163,7 @@ public class GameView extends SurfaceView implements Runnable {
                 circleYPosition = (float)(circleDefaultY + (joystickRadius)*Math.sin(theta));
             }
 
+            //Generate Dolphins
             int randomDolphinNumber = randomNumberGenerator.nextInt(250);
             if (randomDolphinNumber == 249) {
                 for(int i = 0; i < dolphins.size(); i++) {
@@ -163,6 +177,7 @@ public class GameView extends SurfaceView implements Runnable {
                 }
             }
 
+            //Generate Seaweed
             int randomSeaweedNumber = randomNumberGenerator.nextInt(350);
             if (randomSeaweedNumber == 349) {
                 for(int i=0; i < seaweeds.size(); i++) {
@@ -174,23 +189,21 @@ public class GameView extends SurfaceView implements Runnable {
                 }
             }
 
-            for (int i=0; i<seaweeds.size(); i++) {
-                if(seaweeds.get(i).isVisible) {
-                    seaweeds.get(i).update(scrollSpeed, fps);
-                    seaweeds.get(i).getCurrentFrame();
-                }
-            }
-
+            //Update player position
             player.update(fps, circleXPosition, circleYPosition, circleDefaultX, circleDefaultY, scrollSpeed);
             player.getCurrentFrame();
 
+            //Check player oxygen level
             if(player.getOxygenLevel() == 0){
                 gameState = GAME_OVER;
             }
 
+            //Update harpoons
             for(int i = 0; i < harpoons.size(); i++){
                 if(harpoons.get(i).isVisible){
                     harpoons.get(i).update(fps, scrollSpeed);
+
+                    //Check for collision between player and harpoon
                     if (RectF.intersects(harpoons.get(i).getRect(), player.getRect())) {
                         if (!harpoons.get(i).isShot) {
                             harpoons.get(i).deadDolphin = null;
@@ -198,6 +211,8 @@ public class GameView extends SurfaceView implements Runnable {
                             harpoons.get(i).isAngled = false;
                         }
                     }
+
+                    //Check for collision between harpoons and dolphins
                     for (int j=0; j<dolphins.size(); j++) {
                         if(dolphins.get(j).isVisible) {
                             if(RectF.intersects(dolphins.get(j).getRect(), harpoons.get(i).getRect())) {
@@ -208,7 +223,6 @@ public class GameView extends SurfaceView implements Runnable {
                                         harpoons.get(i).isAHit = true;
                                         dolphins.get(j).killHarpoon = harpoons.get(i);
                                         harpoons.get(i).deadDolphin = dolphins.get(j);
-                                        score++;
                                     }
                                 }
                             }
@@ -217,21 +231,12 @@ public class GameView extends SurfaceView implements Runnable {
                 }
             }
 
-            int randomNumber = randomNumberGenerator.nextInt(250);
-            if (randomNumber == 249) {
-                for(int i = 0; i < dolphins.size(); i++) {
-                    if(!dolphins.get(i).isVisible) {
-                        float randomY = randomNumberGenerator.nextFloat()*(screenY-(screenY/10));
-                        dolphins.get(i).generate(randomY);
-                        dolphins.get(i).isDead = false;
-                        break;
-                    }
-                }
-            }
-
+            //Update dolphins
             for (int i = 0; i < dolphins.size(); i++) {
                 if(dolphins.get(i).isVisible()) {
                     dolphins.get(i).update(fps, scrollSpeed);
+
+                    //Check for collision between player and dolphins
                     if(RectF.intersects(dolphins.get(i).getRect(), player.getRect())) {
                         if(!dolphins.get(i).isDead) {
                             gameState = GAME_OVER;
@@ -239,10 +244,21 @@ public class GameView extends SurfaceView implements Runnable {
                             dolphins.get(i).isVisible = false;
                             dolphins.get(i).killHarpoon = null;
                             dolphins.get(i).isDead = false;
+                            score++;
                         }
                     }
                 }
             }
+
+            //Update seaweeds
+            for (int i=0; i<seaweeds.size(); i++) {
+                if(seaweeds.get(i).isVisible) {
+                    seaweeds.get(i).update(scrollSpeed, fps);
+                    seaweeds.get(i).getCurrentFrame();
+                }
+            }
+
+            //Update bubble
             if(bubble.isVisible){
                 bubble.update(scrollSpeed, fps);
                 bubble.getCurrentFrame();
@@ -265,6 +281,8 @@ public class GameView extends SurfaceView implements Runnable {
     public void draw() {
         if (ourHolder.getSurface().isValid()) {
             canvas = ourHolder.lockCanvas();
+
+            //Draw Start Screen
             if (gameState == GAME_START) {
                 canvas.drawColor(Color.argb(255, 105, 255, 217));
                 paint.setColor(Color.argb(255, 0, 29, 77));
@@ -272,60 +290,36 @@ public class GameView extends SurfaceView implements Runnable {
                 canvas.drawText("DEEP FISH", screenX/2-230, screenY/2, paint);
                 paint.setTextSize(60);
                 canvas.drawText("touch screen to start", screenX/2-250, screenY/2+80, paint);
+
+            //Draw Game
             } else if(gameState == GAME_PLAYING) {
                 canvas.drawColor(Color.argb(255, 26, 128, 182));
+
+                //Draw Score
                 paint.setColor(Color.argb(255, 249, 129, 0));
                 paint.setTextSize(45);
                 canvas.drawText("Score: " + score, 20, 40, paint);
 
+                //Draw Oxygen Meter
                 int bubbleMeterPosition = (int) (screenX-screenX/11);
-                Log.v("PLAYER", player.getOxygenLevel()+"");
-                if(player.getOxygenLevel() == 5){
-                 for(int i = 0; i < 5; i++){
-                        canvas.drawBitmap(fillBubbleMeter, bubbleMeterPosition, 40, paint);
-                        bubbleMeterPosition -= 40;
-                    }
-                } else if (player.getOxygenLevel() == 4){
+                for(int i = 0; i < (5-player.getOxygenLevel()); i++) {
                     canvas.drawBitmap(bubbleMeter, bubbleMeterPosition, 40, paint);
                     bubbleMeterPosition -=40;
-                    for(int i = 0; i < 4; i++){
-                        canvas.drawBitmap(fillBubbleMeter, bubbleMeterPosition, 40, paint);
-                        bubbleMeterPosition -= 40;
-                    }
-                } else if (player.getOxygenLevel() == 3){
-                    for(int i=0; i< 2; i++){
-                        canvas.drawBitmap(bubbleMeter, bubbleMeterPosition, 40, paint);
-                        bubbleMeterPosition -= 40;
-                    }
-                    for(int i=0; i < 3; i++){
-                        canvas.drawBitmap(fillBubbleMeter, bubbleMeterPosition, 40, paint);
-                        bubbleMeterPosition -= 40;
-                    }
-                } else if (player.getOxygenLevel() == 2) {
-                    for (int i = 0; i < 3; i++) {
-                        canvas.drawBitmap(bubbleMeter, bubbleMeterPosition, 40, paint);
-                        bubbleMeterPosition -= 40;
-                    }
-                    for (int i = 0; i < 2; i++) {
-                        canvas.drawBitmap(fillBubbleMeter, bubbleMeterPosition, 40, paint);
-                        bubbleMeterPosition -= 40;
-                    }
-                } else {
-                    for (int i = 0; i < 4; i++) {
-                        canvas.drawBitmap(bubbleMeter, bubbleMeterPosition, 40, paint);
-                        bubbleMeterPosition -= 40;
-                    }
-                    for (int i = 0; i < 1; i++) {
-                        canvas.drawBitmap(fillBubbleMeter, bubbleMeterPosition, 40, paint);
-                        bubbleMeterPosition -= 40;
-                    }
                 }
+                 for(int i = 0; i < player.getOxygenLevel(); i++){
+                        canvas.drawBitmap(fillBubbleMeter, bubbleMeterPosition, 40, paint);
+                        bubbleMeterPosition -= 40;
+                 }
 
+                //Draw Joystick
                 canvas.drawCircle(circleDefaultX, circleDefaultY, joystickRadius, paint);
                 paint.setColor(Color.argb(255, 37, 25, 255));
                 canvas.drawCircle(circleXPosition, circleYPosition, (float) (.07*screenY), paint);
+
+                //Draw Player
                 canvas.drawBitmap(player.getBitmap(), player.getFrameToDraw(), player.getRect(), paint);
 
+                //Draw Harpoons
                 for(int i = 0; i < harpoons.size(); i++) {
                     if (harpoons.get(i).isVisible) {
                         if (!harpoons.get(i).isAngled) {
@@ -339,10 +333,7 @@ public class GameView extends SurfaceView implements Runnable {
                     }
                 }
 
-                if(bubble.isVisible){
-                    canvas.drawBitmap(bubble.getBitmap(), bubble.getFrameToDraw(), bubble.getRect(), paint);
-                }
-
+                //Draw Dolphins
                 paint.setColor(Color.argb(255, 255, 0, 234));
                 for (int i = 0; i < dolphins.size(); i++) {
                     if (dolphins.get(i).isVisible) {
@@ -350,13 +341,19 @@ public class GameView extends SurfaceView implements Runnable {
                     }
                 }
 
+                //Draw Seaweed
                 for (int i=0; i<seaweeds.size(); i++) {
                     if(seaweeds.get(i).isVisible) {
                         canvas.drawBitmap(seaweeds.get(i).getBitMap(), seaweeds.get(i).getFrameToDraw(), seaweeds.get(i).getRect(), paint);
                     }
                 }
 
+                //Draw Bubble
+                if(bubble.isVisible){
+                    canvas.drawBitmap(bubble.getBitmap(), bubble.getFrameToDraw(), bubble.getRect(), paint);
+                }
 
+            //Draw game over screen
             } else if(gameState == GAME_OVER) {
                 canvas.drawColor(Color.argb(255, 105, 255, 217));
                 paint.setColor(Color.argb(255, 0, 29, 77));
@@ -393,13 +390,15 @@ public class GameView extends SurfaceView implements Runnable {
                     } else if(gameState == GAME_PLAYING) {
                         int actionIndexDown = motionEvent.getActionIndex();
                         if(motionEvent.getX(actionIndexDown) < screenX/2) {
-                            joystickPointerId = motionEvent.getPointerId(actionIndexDown);
+
+                            //Move joystick if touching left side of screen
                             isMoving = true;
                             player.setFrameLength(200);
                             pointerX = motionEvent.getX(actionIndexDown);
                             pointerY = motionEvent.getY(actionIndexDown);
                         } else {
-                            isShooting = true;
+
+                            //fire harpoon if touching right side of screen
                             for(int i = 0; i < harpoons.size(); i++){
                                 if(!harpoons.get(i).isVisible) {
                                     harpoons.get(i).shoot(player.getX()+(player.getWidth()/3), player.getY()+player.getHeight()/2);
@@ -427,13 +426,10 @@ public class GameView extends SurfaceView implements Runnable {
 
                     int actionIndexUp = motionEvent.getActionIndex();
                     if(motionEvent.getX(actionIndexUp) < screenX/2) {
-                        joystickPointerId = motionEvent.getPointerId(actionIndexUp);
                         isMoving = false;
                         player.setFrameLength(700);
                         pointerX = circleDefaultX;
                         pointerY = circleDefaultY;
-                    } else {
-                        isShooting = false;
                     }
                     break;
             }
